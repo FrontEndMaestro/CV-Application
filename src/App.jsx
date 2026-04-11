21af05c7db8e9a76ee466676295265318257a2d9
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-04-11 16:22:49 +0500

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import Display from "./components/DisplayCV";
+
+function App() {
+  return (
+    <>
+      <h2 className="heading">Cv Builder</h2>
+      <Display></Display>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/DisplayCV.jsx
@@ -0,0 +1,5 @@
+import "../styles/displayCV.css";
+
+export default function DisplayCV() {
+  return <div className="display-container"> &nbsp</div>;
+}
